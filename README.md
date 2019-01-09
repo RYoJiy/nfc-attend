04d8abcd0aaf847ed266004fc986a7d1131ced0a
daisuke <1310370045h@kindai.ac.jp>
2019-01-09 18:21:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Python 2.7.12
+pip 18.1 from /usr/local/lib/python2.7/site-packages/pip (python 2.7)
+
+```
+./main.py
+```
+
+or 
+
+```
+pyton main.py
+```
+
--- main.py
+++ main.py
@@ -8,18 +8,28 @@ import sys
 sys.path.append("/usr/local/lib/python2.7/site-packages")
 # ---
 
+import nfc
 import re
 import time
-import nfc
-import binascii
 
+# message_write
+## 書き込み
 def message_write(sttr):
   f = open('attend.txt', 'a')
   f.write(sttr + "\n")
   f.close()
 
-def on_connect(tag):
-  print "読み込みました！"
+# on_connect
+## fekicaのセットアップ時に発火
+def on_startup(target):
+  def on_startup(targets):
+    print("on_startup()")
+    print targets
+    #return targets
+
+# on_connect
+## felicaにタッチした時に発火
+def on_connected(tag):
   for line in tag.dump(): 
     pattern = r"0000: 30"
     matchLine = re.search(pattern, line)
@@ -27,13 +37,39 @@ def on_connect(tag):
       uId = line.split('|')      
       message = '{ \"time\" : \"' + time.ctime() + '\", \"uId\" : \"' + uId[1][4:14] + '\" }'
       message_write(message)
-      print "学籍番号 : " + uId[1][4:14] + "の出席を確認しました！"
+      print "学籍番号 : " + uId[1][4:14] + " の出席を確認しました！"
       break
+
+  time.sleep(2)
+  print('学生証をかざしてください：')
+
+# on-release
+## felicaを離した時に発火
+def on_released(tag):
+  #print(tag)
+  print('学生証をかざしてください：')
+
+  time.sleep(2)
   
+# main
+## 
 def main():
-  print "学生証をかざしてください:"
-  with nfc.ContactlessFrontend('usb') as clf:
-    clf.connect(rdwr={'on-connect': on_connect})
+  
+  rdwr_options = {
+    #'on-startup': on_startup,
+    'on-connect': on_connected,
+    'on-release': on_released
+  }
+
+  # usb devise との接続を行う
+  clf = nfc.ContactlessFrontend('usb')
+  print(clf)
+
+  print "学生証をかざしてください："
+
+  if clf:
+    while clf.connect(rdwr=rdwr_options):
+      pass
 
 if __name__ == '__main__':
   main()
